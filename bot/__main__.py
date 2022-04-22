e86eda5f7bf9a382eae1f54b8e2a1ebdaaa8cb47
Rk Shaju 612 <91930059+professorop@users.noreply.github.com>
2022-04-22 18:19:16 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -177,7 +177,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='24/7 Mirror Bot Help',
         content=help_string_telegraph,
     )["path"]
 
