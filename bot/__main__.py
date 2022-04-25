648a223cd611aa0e9ca9f402f5b0da81bb556bae
Rk Shaju 612 <91930059+professorop@users.noreply.github.com>
2022-04-25 06:16:43 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -96,7 +96,7 @@ def ping(update, context):
     start_time = int(round(time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update.message)
     end_time = int(round(time() * 1000))
-    editMessage(f'Ping {end_time - start_time} ms', reply)
+    editMessage(f'❖ Ping {end_time - start_time} ms', reply)
 
 
 def log(update, context):
