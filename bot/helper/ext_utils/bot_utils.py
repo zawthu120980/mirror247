e03f2a885aef617510a8d8062fc60dfa98132322
Rk Shaju 612 <91930059+professorop@users.noreply.github.com>
2022-04-29 08:19:08 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -108,7 +108,7 @@ def get_progress_bar_string(status):
     cFull = p // 8
     p_str = '█' * cFull
     p_str += '▒' * (12 - cFull)
-    p_str = f"<b>Progress: <b/>{p_str}"
+    p_str = f"<b>Progress: </b>{p_str}"
     return p_str
 
 def get_readable_message():
